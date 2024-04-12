f55e5d604435f85d34d35287adc1601749ff3c62
antoinehage612 <hypercoffin612@gmail.com>
2024-04-12 16:34:10 +0300

    hello world

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ const sendVerificationEmail = async (email, verificationToken) => {
     from: "amazon.com",
     to: email,
     subject: "Email Verification",
-    text: `Please click the following link to verify your email: http://localhost:8000/verify/${verificationToken}`,
+    text: `Please click the following link to verify your email: https://amazon-reactnative-backend.onrender.com/verify/${verificationToken}`,
   };
 
   // Send the email
